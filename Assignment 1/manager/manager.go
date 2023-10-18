f9ede015052f1d1272c5340164a64a5e9bfb57b8
Shyngys03 <vip.sh3541@gmail.com>
2023-10-18 20:00:27 +0600

    1st assignment

---
+++ Assignment 1/manager/manager.go
@@ -0,0 +1,33 @@
+package manager
+
+import "Assignment 1/employee"
+
+type Manager struct {
+    position string
+    salary   float64
+    address  string
+}
+
+func (m *Manager) GetPosition() string {
+    return m.position
+}
+
+func (m *Manager) SetPosition(pos string) {
+    m.position = pos
+}
+
+func (m *Manager) GetSalary() float64 {
+    return m.salary
+}
+
+func (m *Manager) SetSalary(sal float64) {
+    m.salary = sal
+}
+
+func (m *Manager) GetAddress() string {
+    return m.address
+}
+
+func (m *Manager) SetAddress(addr string) {
+    m.address = addr
+}
