4af4efb80d3ae01269afdbd2c891b7bb74d9e519
Shyngys03 <vip.sh3541@gmail.com>
2023-12-28 16:58:16 +0600

    project

--- midterm/project/internal/data/drills.go
+++ midterm/project/internal/data/drills.go
@@ -14,12 +14,12 @@ import (
 )
 
 type Drill struct {
-	ID int64			`json: "id"`
-	Weight float32		`json: "weight"`
-	Name string			`json: "name"`
-	CableLength float32 `json: "length"`
-	Worktime int32 		`json: "worktime"`
-	ChuckDiameter int32 `json: "diameter"`
+	ID int64			`json:"id"`
+	Weight float32		`json:"weight"`
+	Name string			`json:"name"`
+	CableLength float32 `json:"length"`
+	Worktime int32 		`json:"worktime"`
+	ChuckDiameter int32 `json:"diameter"`
 }
 
 
